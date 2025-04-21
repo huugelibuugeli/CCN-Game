f7617622873647b3209a18d240c7f1fdfe5acf28
VladSapozhnikov <vsapozhn@kent.edu>
2025-04-21 20:31:52 +0000

    Switched Host IP

--- client/client.py
+++ client/client.py
@@ -7,7 +7,7 @@ def client_program():
     print("trying to connect to server")
 
     #REMEMBER TO CHANGE THIS IP ADDRESS TO YOUR SERVER IP ADDRESS
-    host = "10.0.2.221"
+    host = "127.0.0.1"
     port = 5000  # socket server port number
 
     client_socket = socket.socket()  # instantiate
